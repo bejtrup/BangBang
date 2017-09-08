5b0fe569d584e6e2fdb89d06cd45ac4c4bad08b5
daniel <bejtrup@gmail.com>
2017-09-08 21:54:56 +0200

    hello

---
+++ bangbang.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimun-scale=1.0, initial-scale=1.0" />
+		<title>Bang!Bang!</title>
+                <link rel="stylesheet" type="text/css" href="css/bangbang.css" />
+                
+                <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+<!--                <meta http-equiv="refresh" content="1" >-->
+           
+    </head>
+    <body>
+
+        <div class="wrapper">
+            
+            <div class="left">
+                <img src="img/hand_left.png">
+                <img style="z-index: 4;" src="img/fingers_left.png">
+            </div>
+            <div class="right">
+                <img src="img/hand_right.png">
+            </div>
+            <div class="bang">BANG!</div>
+        </div>
+        
+    </body>
+</html>
+
+                        
